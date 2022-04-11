b45d8015766e19b4193531571254836da396db14
TR742 <Tyrich1442@gmail.com>
2022-04-10 22:05:29 -0700

    API calls working to consolelog

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const APIKey = "acf8aa8162d6224707eab34581845c12";
+let apiKey = "acf8aa8162d6224707eab34581845c12";
+let userCity = document.getElementById('city');
+let searchBtn = document.getElementById('search');
 
+searchBtn.addEventListener('click', getApi);
 
-function getAPI() {
-    let requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=acf8aa8162d6224707eab34581845c12";
+function getApi() {
+    let selectedCity = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.value + "&appid=acf8aa8162d6224707eab34581845c12";
 
-    fetch(requestUrl)
+    fetch(selectedCity)
         .then(function (response) {
+            console.log(response);
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-        })
-};
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            let cityWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=alerts&appid=acf8aa8162d6224707eab34581845c12";
 
-getAPI();
+            fetch(cityWeather)
+                .then(function (newResponse) {
+                    console.log(newResponse);
+                    return newResponse.json();
+                })
+                .then(function (newData) {
+                    console.log(newData);
+                });
+        });
+};
